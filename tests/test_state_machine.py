11879f75d821cea7b723bae1b4430fab63a5f434
Stefan Smith <stefan.smith@gousto.co.uk>
2024-10-07 12:56:20 +0100

    State machine invokes a second Lambda function to double the result

--- tests/test_state_machine.py
+++ tests/test_state_machine.py
@@ -18,12 +18,17 @@ def test_state_machine(mocking_engine: AWSResourceMockingEngine, resource_driver
         'InputTransformerFunction',
         lambda event: {'number': event['data']['number'] + 1}
     )
+    doubler_function = mocking_engine.mock_a_lambda_function(
+        'DoublerFunction',
+        lambda event: {'number': event['number'] * 2}
+    )
 
     state_machine = resource_driver.get_stack_machine("ExampleStateMachine::StateMachine")
 
     final_state = state_machine.execute({'input': {'data': {'number': 1}}})
 
-    assert json.loads(final_state['output']) == {'result': {'number': 2}}
+    assert json.loads(final_state['output']) == {'result': {'number': 4}}
 
     # TODO: Tolerate eventual consistency
     input_transformer_function.assert_called_with({'data': {'number': 1}})
+    doubler_function.assert_called_with({'number': 2})
