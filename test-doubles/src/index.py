0487ffea67f4a219f70405ca7a06da8296db5e16
Stefan Smith <stefan.smith@gousto.co.uk>
2024-10-17 08:56:31 +0100

    Don't delete result messages destined for other Lambda function invocations
    Also, make these messages visible to other consumers ASAP

--- test-doubles/src/index.py
+++ test-doubles/src/index.py
@@ -30,29 +30,29 @@ def handler(event, _):
         print('Waiting for message...')
         results_queue_url = os.environ['RESULTS_QUEUE_URL']
 
-        i = 20
         result = sqs_client.receive_message(
             QueueUrl=results_queue_url,
             MessageSystemAttributeNames=['All'],
             MessageAttributeNames=['All'],
             MaxNumberOfMessages=1,
-            WaitTimeSeconds=i,
+            WaitTimeSeconds=20,
         )
 
         if 'Messages' in result:
             for message in result['Messages']:
                 print(f'Message received: {message}')
                 message_group_id = message['Attributes']['MessageGroupId']
+                receipt_handle = message['ReceiptHandle']
 
                 if message_group_id == lambda_function_name:
                     result_message_payload = json.loads(message['Body'])
                     lambda_execution_environment_id = result_message_payload['executionEnvironmentId']
 
                     if lambda_execution_environment_id == os.environ['AWS_LAMBDA_LOG_STREAM_NAME']:
-                        try:
-                            lambda_function_invocation_id = result_message_payload['invocationId']
+                        lambda_function_invocation_id = result_message_payload['invocationId']
 
-                            if lambda_function_invocation_id == invocation_id:
+                        if lambda_function_invocation_id == invocation_id:
+                            try:
                                 lambda_function_result = json.loads(result_message_payload['result'])
 
                                 print(f"Result received for {lambda_function_name} invocation "
@@ -60,15 +60,27 @@ def handler(event, _):
                                       f"with invocation ID {lambda_function_invocation_id} : {lambda_function_result}")
 
                                 return lambda_function_result
-                        finally:
-                            try:
-                                receipt_handle = message['ReceiptHandle']
-                                print(f"Deleting message {json.dumps(result_message_payload)}...")
-                                sqs_client.delete_message(
-                                    QueueUrl=results_queue_url,
-                                    ReceiptHandle=receipt_handle
-                                )
-                            except ClientError as e:
-                                print(f"Failed to delete message: {e}")
+                            finally:
+                                try:
+                                    print(f"Deleting message {json.dumps(result_message_payload)}...")
+                                    sqs_client.delete_message(
+                                        QueueUrl=results_queue_url,
+                                        ReceiptHandle=receipt_handle
+                                    )
+                                except ClientError as e:
+                                    print(f"Failed to delete message: {e}")
+
+                print(
+                    "Message received for a different Lambda function, execution environment or invocation. "
+                    "Skipping and making it available to other consumers..."
+                )
+
+                # Make message immediately available to other consumers
+                # TODO: delete messages from previous test runs that would otherwise block processing the message group
+                sqs_client.change_message_visibility(
+                    QueueUrl=results_queue_url,
+                    ReceiptHandle=receipt_handle,
+                    VisibilityTimeout=0
+                )
         else:
             print('No messages received')
