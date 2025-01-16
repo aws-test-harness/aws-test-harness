1170c2441da31436595eeab7ae8d87380ed24318
Stefan Smith <stefan.smith@gousto.co.uk>
2025-01-16 16:01:59 +0000

    Improve scalability of Lambda test doubles
    Shard SQS messages by Lambda execution environment, rather than by Lambda function name

--- infrastructure/templates/test-doubles/src/index.py
+++ infrastructure/templates/test-doubles/src/index.py
@@ -28,7 +28,7 @@ def handler(event, _):
 
     sqs_client.send_message(
         QueueUrl=os.environ['EVENTS_QUEUE_URL'],
-        MessageGroupId=lambda_function_name,
+        MessageGroupId=lambda_execution_environment_id,
         MessageBody=json.dumps(message_payload),
         MessageAttributes={
             'MockingSessionId': {
@@ -56,37 +56,36 @@ def handler(event, _):
                 receipt_handle = message['ReceiptHandle']
 
                 if message['MessageAttributes']['MockingSessionId']['StringValue'] == mocking_session_id:
-                    if message['Attributes']['MessageGroupId'] == lambda_function_name:
+                    if message['Attributes']['MessageGroupId'] == lambda_execution_environment_id:
                         result_message_payload = json.loads(message['Body'])
 
-                        if result_message_payload['executionEnvironmentId'] == lambda_execution_environment_id:
-                            if result_message_payload['invocationId'] == invocation_id:
-                                try:
-                                    raise_exception = result_message_payload.get('raiseException')
+                        if result_message_payload['invocationId'] == invocation_id:
+                            try:
+                                raise_exception = result_message_payload.get('raiseException')
 
-                                    if raise_exception:
-                                        exception_message = result_message_payload['exceptionMessage']
+                                if raise_exception:
+                                    exception_message = result_message_payload['exceptionMessage']
 
-                                        print(
-                                            f"Exception to throw (with message {exception_message}) received for "
-                                            f"{lambda_function_name} invocation in execution environment "
-                                            f"{lambda_execution_environment_id} with invocation ID {invocation_id}"
-                                        )
+                                    print(
+                                        f"Exception to throw (with message {exception_message}) received for "
+                                        f"{lambda_function_name} invocation in execution environment "
+                                        f"{lambda_execution_environment_id} with invocation ID {invocation_id}"
+                                    )
 
-                                        raise Exception(exception_message)
-                                    else:
-                                        lambda_function_result = json.loads(result_message_payload['result'])
+                                    raise Exception(exception_message)
+                                else:
+                                    lambda_function_result = json.loads(result_message_payload['result'])
 
-                                        print(
-                                            f"Result received for {lambda_function_name} invocation "
-                                            f"in execution environment {lambda_execution_environment_id} "
-                                            f"with invocation ID {invocation_id} : {lambda_function_result}"
-                                        )
+                                    print(
+                                        f"Result received for {lambda_function_name} invocation "
+                                        f"in execution environment {lambda_execution_environment_id} "
+                                        f"with invocation ID {invocation_id} : {lambda_function_result}"
+                                    )
 
-                                        return lambda_function_result
-                                finally:
-                                    print(f"Deleting consumed message {json.dumps(result_message_payload)}...")
-                                    delete_message(receipt_handle, results_queue_url)
+                                    return lambda_function_result
+                            finally:
+                                print(f"Deleting consumed message {json.dumps(result_message_payload)}...")
+                                delete_message(receipt_handle, results_queue_url)
 
                     print(
                         "Message received for a different Lambda function, execution environment or invocation. "
