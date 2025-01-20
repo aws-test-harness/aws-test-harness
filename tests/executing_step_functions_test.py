d4adc517160e19ac634db7b7bc83a1856fc360cb
Stefan Smith <stefan.smith@gousto.co.uk>
2025-01-20 17:43:45 +0000

    Acceptance test for testing a state machine and detecting whether it succeeds or fails

---
+++ tests/executing_step_functions_test.py
@@ -0,0 +1,94 @@
+import json
+import logging
+import os
+from logging import Logger
+from typing import Dict
+
+import pytest
+from boto3 import Session
+
+from support.cloudformation_driver import CloudFormationDriver
+from support.state_machine_driver import StateMachineDriver
+
+
+@pytest.fixture(scope="session")
+def logger():
+    return logging.getLogger()
+
+
+@pytest.fixture(scope="session")
+def test_configuration():
+    configuration_file_path = os.path.join(os.path.dirname(__file__), 'config.json')
+
+    with open(configuration_file_path, 'r') as f:
+        return json.load(f)
+
+
+@pytest.fixture(scope="session")
+def boto_session(test_configuration: Dict[str, str]):
+    return Session(profile_name=test_configuration['awsProfile'])
+
+
+@pytest.fixture(scope="session")
+def cloudformation_stack_name(test_configuration: Dict[str, str]):
+    return test_configuration['cfnStackName']
+
+
+@pytest.fixture(scope="session")
+def cloudformation_driver(boto_session: Session, logger: Logger):
+    return CloudFormationDriver(boto_session.client('cloudformation'), logger)
+
+
+@pytest.fixture(scope="session")
+def state_machine_driver(cloudformation_driver: CloudFormationDriver, boto_session: Session, logger: Logger):
+    return StateMachineDriver(cloudformation_driver, boto_session, logger)
+
+
+@pytest.fixture(scope="session", autouse=True)
+def before_all(cloudformation_driver: CloudFormationDriver, cloudformation_stack_name: str):
+    state_machine_definition = dict(
+        StartAt='SetResult',
+        States=dict(
+            SetResult=dict(Type='Pass', Parameters={'result.$': '$.input'}, End=True)
+        )
+    )
+
+    cloudformation_driver.ensure_stack_is_up_to_date(
+        cloudformation_stack_name,
+        dict(
+            AWSTemplateFormatVersion='2010-09-09',
+            Transform='AWS::Serverless-2016-10-31',
+            Resources=dict(
+                StateMachine=dict(
+                    Type='AWS::Serverless::StateMachine',
+                    Properties=dict(Definition=state_machine_definition)
+                )
+            )
+        )
+    )
+
+
+def test_detecting_a_successful_step_function_execution(state_machine_driver: StateMachineDriver,
+                                                        cloudformation_stack_name: str):
+    execution_result = state_machine_driver.start_execution({'input': 'Any input'}, 'StateMachine',
+                                                            cloudformation_stack_name)
+
+    assert execution_result.status == 'SUCCEEDED'
+
+    assert execution_result.output is not None
+    assert json.loads(execution_result.output) == {"result": "Any input"}
+
+
+def test_detecting_a_failed_step_function_execution(state_machine_driver: StateMachineDriver,
+                                                    cloudformation_stack_name: str):
+    execution_result = state_machine_driver.start_execution({}, 'StateMachine',
+                                                            cloudformation_stack_name)
+
+    assert execution_result.status == 'FAILED'
+
+    cause = execution_result.cause
+    assert cause is not None
+    assert "JSONPath '$.input' specified for the field 'result.$' could not be found in the input" in cause
+
+    error = execution_result.error
+    assert error == 'States.Runtime'
---
+++ tests/support/cloudformation_driver.py
@@ -0,0 +1,50 @@
+import json
+from logging import Logger
+from typing import Dict
+
+from botocore.exceptions import ClientError
+from mypy_boto3_cloudformation import CloudFormationClient
+
+
+class CloudFormationDriver:
+    def __init__(self, cloudformation_client: CloudFormationClient, logger: Logger):
+        self.__cloudformation_client = cloudformation_client
+        self.__logger = logger
+
+    def get_physical_resource_id(self, cloudformation_stack_name, logical_resource_id):
+        describe_stack_resource_result = self.__cloudformation_client.describe_stack_resource(
+            StackName=cloudformation_stack_name,
+            LogicalResourceId=logical_resource_id
+        )
+
+        return describe_stack_resource_result['StackResourceDetail']['PhysicalResourceId']
+
+    def ensure_stack_is_up_to_date(self, cloudformation_stack_name: str, stack_template_data: Dict[str, any]):
+        self.__logger.info('Ensuring CloudFormation stack is up-to-date...')
+
+        try:
+            self.__cloudformation_client.create_stack(
+                StackName=cloudformation_stack_name,
+                TemplateBody=json.dumps(stack_template_data),
+                Capabilities=['CAPABILITY_IAM', 'CAPABILITY_AUTO_EXPAND'],
+                OnFailure='DELETE'
+            )
+
+            create_stack_waiter = self.__cloudformation_client.get_waiter('stack_create_complete')
+            create_stack_waiter.wait(StackName=cloudformation_stack_name, WaiterConfig=dict(Delay=3, MaxAttempts=30))
+
+        except ClientError as client_error:
+            # noinspection PyUnresolvedReferences
+            if client_error.response['Error']['Code'] != 'AlreadyExistsException':
+                raise client_error
+
+            self.__cloudformation_client.update_stack(
+                StackName=cloudformation_stack_name,
+                TemplateBody=json.dumps(stack_template_data),
+                Capabilities=['CAPABILITY_IAM', 'CAPABILITY_AUTO_EXPAND'],
+            )
+
+            create_stack_waiter = self.__cloudformation_client.get_waiter('stack_update_complete')
+            create_stack_waiter.wait(StackName=cloudformation_stack_name, WaiterConfig=dict(Delay=3, MaxAttempts=30))
+
+        self.__logger.info('CloudFormation stack is up-to-date.')
