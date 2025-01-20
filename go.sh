0cff42c2768654374a6aab13c211c1a7eb92b92c
Stefan Smith <stefan.smith@gousto.co.uk>
2025-01-20 21:14:04 +0000

    Lint with ruff

--- go.sh
+++ go.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
+echo Initializing virtual environment...
 uv venv --allow-existing --seed
+echo
+
+echo Synchronizing virtual environment with lock file...
 uv sync --frozen
+echo
--- lint.sh
+++ lint.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
+echo Typing checking...
 uv run --frozen mypy .
+echo
+
+echo Linting...
+uv run --frozen ruff check .
+echo
