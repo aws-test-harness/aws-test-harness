ad47e95d50d53888eea7b06862bf809780b53785
Stefan Smith <indiansummersky@gmail.com>
2025-02-02 14:32:24 +0000

    Use CloudFormation resource rather than client

--- languages/python/src/aws_test_harness/cloudformation/resource_registry.py
+++ languages/python/src/aws_test_harness/cloudformation/resource_registry.py
@@ -1,16 +1,12 @@
 from boto3 import Session
-from mypy_boto3_cloudformation.client import CloudFormationClient
+from mypy_boto3_cloudformation.service_resource import CloudFormationServiceResource
 
 
 class ResourceRegistry:
     def __init__(self, stack_name: str, boto_session: Session):
         self.__stack_name = stack_name
-        self.__cloudformation_client: CloudFormationClient = boto_session.client('cloudformation')
+        self.__boto_resource: CloudFormationServiceResource = boto_session.resource('cloudformation')
 
     def get_physical_resource_id(self, logical_resource_id: str) -> str:
-        describe_stack_resource_result = self.__cloudformation_client.describe_stack_resource(
-            StackName=self.__stack_name,
-            LogicalResourceId=logical_resource_id
-        )
-
-        return describe_stack_resource_result['StackResourceDetail']['PhysicalResourceId']
+        stack_resource = self.__boto_resource.StackResource(self.__stack_name, logical_resource_id)
+        return stack_resource.physical_resource_id
