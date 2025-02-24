80bc8e0149edd553614b9860b1e1a96cf23a0faf
Stefan Smith <indiansummersky@gmail.com>
2025-02-24 17:41:17 +0000

    Dedicated test script for acceptance tests

--- infrastructure/acceptance-tests/test.sh
+++ infrastructure/acceptance-tests/test.sh
@@ -4,5 +4,6 @@ set -o nounset -o errexit -o pipefail
 
 script_directory_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-"${script_directory_path}/macros/test-doubles/test.sh"
-"${script_directory_path}/acceptance-tests/test.sh"
+echo Running infrastructure acceptance tests...
+uv run --directory "${script_directory_path}" pytest "${script_directory_path}/tests"
+echo
