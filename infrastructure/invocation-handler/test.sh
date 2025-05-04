2042605d09cd7e6b4287aa4afb1a659af345ddfd
Stefan Smith <indiansummersky@gmail.com>
2025-05-04 12:32:42 +0100

    Invocation handler tests share same source tree

--- infrastructure/invocation-handler/test.sh
+++ infrastructure/invocation-handler/test.sh
@@ -5,9 +5,9 @@ set -o nounset -o errexit -o pipefail
 script_directory_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 echo Running invocation handler integration tests...
-uv run --isolated --directory "${script_directory_path}" pytest "${script_directory_path}/tests/integration"
+uv run --isolated --directory "${script_directory_path}" pytest "${script_directory_path}/tests/invocation_handler_tests/integration"
 echo
 
 echo Running invocation handler acceptance tests...
-uv run --isolated --directory "${script_directory_path}" pytest "${script_directory_path}/tests/acceptance"
+uv run --isolated --directory "${script_directory_path}" pytest "${script_directory_path}/tests/invocation_handler_tests/acceptance"
 echo
