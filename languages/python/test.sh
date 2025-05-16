495a2d00cfbf29f1a278ce99a22f8bf0c21aacd6
Stefan Smith <indiansummersky@gmail.com>
2025-05-16 11:33:34 +0100

    Run python tests in order of increasing duration

--- languages/python/test.sh
+++ languages/python/test.sh
@@ -4,6 +4,14 @@ set -o nounset -o errexit -o pipefail
 
 script_directory_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-echo Running Python library tests...
-uv run --isolated --directory "${script_directory_path}" pytest "${script_directory_path}/tests"
+echo Running Python library unit tests...
+uv run --isolated --directory "${script_directory_path}" pytest "${script_directory_path}/tests/aws_test_harness_tests/unit"
+echo
+
+echo Running Python library integration tests...
+uv run --isolated --directory "${script_directory_path}" pytest "${script_directory_path}/tests/aws_test_harness_tests/integration"
+echo
+
+echo Running Python library acceptance tests...
+uv run --isolated --directory "${script_directory_path}" pytest "${script_directory_path}/tests/aws_test_harness_tests/acceptance"
 echo
