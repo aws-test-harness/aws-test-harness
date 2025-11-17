d5a7949ae3599b5029b6bdbecaa1fbb17a93ef1d
Stefan Smith <stefan.smith@gousto.co.uk>
2025-11-17 21:57:56 +0000

    Run subset of test suites in parallel

--- test.sh
+++ test.sh
@@ -2,11 +2,13 @@
 
 set -o nounset -o errexit -o pipefail
 
+search_path="${1:-.}"
+
 cleanup() {
     pkill -TERM -g $$ || true
 }
 trap cleanup SIGINT SIGTERM EXIT
 
-test_paths="$(find ./**/* -type f -name test.sh -exec dirname {} + | xargs -0)"
+test_paths="$(find "${search_path}"/**/* -type f -name test.sh -exec dirname {} + | xargs -0)"
 # shellcheck disable=SC2086
 bin/parallel-test ${test_paths}
